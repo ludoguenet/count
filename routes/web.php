100cddb2b872fe4a23bda2457e5e3f4e28ea0de3
ludoguenet <ludovicguenet@gmx.com>
2024-06-16 13:02:39 +0200

    fix: count

--- routes/web.php
+++ routes/web.php
@@ -1,62 +1,64 @@
 <?php
 
+declare(strict_types=1);
+
 use App\Models\Category;
 use App\Models\Product;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/simple-count', function () {
+Route::get('simple-count', function () {
     ray()->showQueries();
 
     return [
-        'Electronics' => Product::query()->whereHas('categories', function ($query) {
-            $query->where('categories.name', 'Electronics');
-        })->count(),
-        'Health & Beauty' => Product::query()->whereHas('categories', function ($query) {
-            $query->where('categories.name', 'Health & Beauty');
-        })->count(),
-        'Home & Kitchen' => Product::query()->whereHas('categories', function ($query) {
-            $query->where('categories.name', 'Home & Kitchen');
-        })->count(),
+      'Electronics' => Product::query()->whereHas('categories', function ($query) {
+          $query->where('name', 'Electronics');
+      })->count(),
+        'Health' => Product::query()->whereHas('categories', function ($query) {
+          $query->where('name', 'Health');
+      })->count(),
+        'Home' => Product::query()->whereHas('categories', function ($query) {
+          $query->where('name', 'Home');
+      })->count(),
     ];
 });
 
-Route::get('/filter', function () {
+Route::get('filter', function () {
     ray()->showQueries();
 
-    $products = Product::query()->with('categories')->get();
+    $products = Product::with('categories')->get();
 
     return [
         'Electronics' => $products->filter(fn ($product) => $product->categories->contains('name', 'Electronics'))->count(),
-        'Health & Beauty' => $products->filter(fn ($product) => $product->categories->contains('name', 'Health & Beauty'))->count(),
-        'Home & Kitchen' => $products->filter(fn ($product) => $product->categories->contains('name', 'Home & Kitchen'))->count(),
+        'Health' => $products->filter(fn ($product) => $product->categories->contains('name', 'Health'))->count(),
+        'Home' => $products->filter(fn ($product) => $product->categories->contains('name', 'Home'))->count(),
     ];
 });
 
-Route::get('/categories', function () {
+Route::get('categories', function () {
     ray()->showQueries();
 
     $categories = Category::query()->withCount('products')->get()->keyBy('name');
 
     return [
-        'Electronics' => $categories['Electronics']->products_count,
-        'Health & Beauty' => $categories['Health & Beauty']->products_count,
-        'Home & Kitchen' => $categories['Home & Kitchen']->products_count,
+      'Electronics' => $categories['Electronics']->products_count,
+      'Health' => $categories['Health']->products_count,
+      'Home' => $categories['Home']->products_count,
     ];
 });
 
-Route::get('/raw', function () {
+Route::get('raw', function () {
     ray()->showQueries();
 
-    $categories = DB::select("SELECT
-         COUNT(CASE WHEN category_id = 1 THEN 1 END) AS electronics,
-         COUNT(CASE WHEN category_id = 2 THEN 1 END) AS health,
-         COUNT(CASE WHEN category_id = 3 THEN 1 END) AS kitchen
+    $db = DB::select("SELECT
+        COUNT(CASE WHEN category_id = 1 THEN 1 END) AS electronics_count,
+        COUNT(CASE WHEN category_id = 2 THEN 1 END) AS health_count,
+        COUNT(CASE WHEN category_id = 3 THEN 1 END) AS home_count
     FROM category_product")[0];
 
     return [
-        'Electronics' => $categories->electronics,
-        'Health & Beauty' => $categories->health,
-        'Home & Kitchen' => $categories->kitchen,
+        'Electronics' => $db->electronics_count,
+        'Health' => $db->health_count,
+        'Home' => $db->home_count,
     ];
 });
